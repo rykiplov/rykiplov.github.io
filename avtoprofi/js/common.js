5d00005c9f766b3b16ede17c5549d9acdb7eb113
Tymur Yunusov <rykiplov@yahoo.com>
2018-01-19 18:11:22 +0200

    preloader

--- avtoprofi/js/common.js
+++ avtoprofi/js/common.js
@@ -269,8 +269,8 @@ $("body").niceScroll({
 /*-----------------------------------------------------------------------------------*/
 jQuery(window).load(function () {
 	//Preloader
-	setTimeout("jQuery('#preloader').animate({'opacity' : '0'},300,function(){jQuery('#preloader').hide()})",800);
-	setTimeout("jQuery('.preloader_hide, .selector_open').animate({'opacity' : '1'},500)",800);
+	setTimeout("jQuery('#preloader').animate({'opacity' : '0'},300,function(){jQuery('#preloader').hide()})",500);
+	setTimeout("jQuery('.preloader_hide, .selector_open').animate({'opacity' : '1'},500)",500);
 	setTimeout("jQuery('footer').animate({'opacity' : '1'},500)",2000);
 
 });
